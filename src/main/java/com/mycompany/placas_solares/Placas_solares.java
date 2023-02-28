e88610f4002db325951ee4a4d14ab73a7cf2c705
Sergioo-D <sergio.dominguez.garcia@ieselcalamot.com>
2023-02-28 22:55:20 +0100

    Agrego el metodo addCasa

--- src/main/java/com/mycompany/placas_solares/Placas_solares.java
+++ src/main/java/com/mycompany/placas_solares/Placas_solares.java
@@ -10,6 +10,18 @@ public class Placas_solares {
     public static void main(String[] args) throws IOException {
      ArrayList<Casa> LasCasas = new ArrayList<Casa>();
      BufferedReader terminal = new BufferedReader(new InputStreamReader(System.in));
+     public void addCasa(String nif, String nom, double superficie) {
+    if (superficie <= 10) {
+        System.out.println("Error: la superfície ha de ser més gran que 10.");
+        return;
+    }
+    
+    Casa novaCasa = new Casa(nif, nom, superficie);
+    novaCasa.activarInterruptorGeneral();
+    
+    // Afegir nova casa a l'empresa
+    empresa.registrarCasa(novaCasa);
+}
      String comanda = "";
      do{
          System.out.print("> ");
